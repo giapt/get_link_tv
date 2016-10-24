33301aef5cd26b337cb322c6d609dbde1f20ba5c
giapt <giapt@ows.vn>
2016-10-25 00:38:44 +0700

    new update to v1.1

--- getlinklivetv_v1.1.py
+++ getlinklivetv_v1.1.py
@@ -1,41 +1,58 @@
-import mechanize
+from urlparse import urlsplit
+from urlparse import urlparse
 import urllib
-import json
+import os
 from bs4 import BeautifulSoup
 
 r = urllib.urlopen('http://htvonline.com.vn/livetv').read()
 soup = BeautifulSoup(r)
 
 divTag = soup.find_all("div", {"class":"channels_htvc"})
-list_link2 = []
+list_link = []
 for tag in divTag:
     divChildtag = tag.findAll("div", class_="view_title2")
-    # print divChildtag
     title=divChildtag[0].findAll("div")
-    # print title[0].text
-    # print "\n"
     tdTags = tag.find_all("a")
     for tag in tdTags:
         if tag.get('href') != None:
-          # print tag.get('href')
-          # print "\n"
-          list_link2.append(tag.get('href'))
+          list_link.append(tag.get('href'))
           pass
 
-r2 = urllib.urlopen(list_link2[0]).read()
-soup2 = BeautifulSoup(r)
-divTag2 = soup2.find_all("div", id="play_video")
-print divTag2[0].get('data-source')
 
 
-# list_link = []
-# for tag in soup.find_all("a"):
-#   if type(tag.get('href')) == str:
-#     list_link.append(tag.get('href'))
-#     pass
+# print list_link
+def write():
+    print('Creating new text file')
 
-# matching = [s for s in list_link if 'vtv' in s]
-# for link in matching:
-#     print link
-#     print "\n"
+    name = raw_input('Enter name of text file: ')+'.m3u'
 
+    try:
+        file = open(name,'w')
+        file.write("#EXTM3U\n")
+        length = len(list_link)
+        for x in xrange(1,length):
+          print x
+          print list_link[x]
+          r2 = urllib.urlopen(list_link[x]).read()
+          soup2 = BeautifulSoup(r)
+          divTag2 = soup2.find_all("div", id="play_video")
+          result = divTag2[0].get('data-source')
+          print result
+          if result is not None:
+            url_parts = urlparse(result)
+            path_parts = url_parts[2].rpartition('/')
+            file.write("#EXTINF:-1,1,"+os.path.splitext(path_parts[2])[0].upper()
+            + "\n")
+            file.write(result + "\n\n")
+            pass
+          r2 = ""
+          divTag2 = []
+          soup2 = ""
+          result = ""
+          pass
+
+        file.close()
+
+    except Exception,e: print str(e)
+
+write()
